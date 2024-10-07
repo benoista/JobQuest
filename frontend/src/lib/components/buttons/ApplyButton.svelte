4cdaa2816090fb27f3d048b0e18f3d84243290ac
germain <e207384s>
2024-10-07 20:15:09 +0200

    refactor frontend

--- frontend/src/lib/components/buttons/ApplyButton.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts"></script>
-
-<button class="btn btn-primary"> Apply </button>
-
-<style></style>
--- frontend/src/lib/components/buttons/LearnMoreButton.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    function handleClick(){
-        console.log('Learn More clicked');
-    }
-</script>
-
-
-<button class="btn btn-outline-info" on:click={() => handleClick()}>Learn More</button>
