3c15deb145b8b9ed0565db30eae9a6cf27f1e18f
germain <germaindev123@gmail.com>
2024-10-11 17:25:17 +0200

    add apply form not finished

--- frontend/src/lib/components/buttons/ApplyButton.svelte
+++ frontend/src/lib/components/buttons/ApplyButton.svelte
@@ -1,5 +1,14 @@
 <script>
-    import {Button} from "$lib/shadcncomponents/ui/button";
+    import {Button, buttonVariants} from "$lib/shadcncomponents/ui/button";
+    import * as Dialog from "$lib/shadcncomponents/ui/dialog";
+    import ApplyForm from "$lib/components/forms/ApplyForm.svelte";
 </script>
 
-<Button> Apply </Button>
+
+<Dialog.Root>
+    <Dialog.Trigger class={buttonVariants({ variant: "outline" })}> Apply </Dialog.Trigger>
+
+    <Dialog.Content class="w-full sm:w-full md:w-full lg:w-full max-w-[80%]">
+        <ApplyForm></ApplyForm>
+    </Dialog.Content>
+</Dialog.Root>
