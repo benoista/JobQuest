0019a31cdb996adc50d65926d87b4000831e36e7
Alexandre <alexandre.benoist@epitech.eu>
2024-10-14 16:04:12 +0200

    fix

--- frontend/src/lib/controllers/applicationController.ts
+++ frontend/src/lib/controllers/applicationController.ts
@@ -9,8 +9,33 @@ export async function apply(name: string, firstName: string, email: string, adve
         },
         body: JSON.stringify({name, firstName, email, advertId})
     });
-    return response.json();
-}
+    if (!response.ok){
+        switch(response.status){
+            case 400:
+                alert('Invalid request');
+                break;
+            case 401:
+                alert('Unauthorized');
+                break;
+            case 404:
+                alert('Advert not found');
+                break;
+            case 409:
+                alert('Already applied');
+                break;
+            default:
+                alert('Something went wrong');
+                break;
+        }
+    } else{
+        alert('Application sent');
+        return await response.json();
+    };
+};
+
+
+
+
 
 export async function myapplication(){
     try {
